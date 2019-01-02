ae66feb21567e3f3b316263f6ca4df7b33151e87
Chinmay <wadgaonc@oregonstate.edu>
2019-01-02 00:54:51 -0800

    Refactored and created getCenter() helper function.

--- algo.py
+++ algo.py
@@ -8,7 +8,7 @@ import string;
 # side_5 = [];
 # side_6 = [];
 
-cube = [['G','G','G','G','G','G','G','G','G'],['O','O','O','O','O','O','O','O','O'],['R','R','R','R','R','R','R','R','R'],['Y','Y','Y','Y','Y','Y','Y','Y','Y'],['W','W','W','W','W','W','W','W','W'],['B','B','B','B','B','B','B','B','B']];
+cube = [['G','G','G','G','G','G','G','G','G'],['O','O','O','O','O','O','O','O','O'],['R','R','R','R','R','R','R','R','R'],['Y','Y','Y','Y','Y','Y','Y','Y','Y'],['W','W','W','W','W','W','W','W','W'],['B','B','B','B','B','B','B','B','B']]
 
 # string_1= raw_input('Type Side1 colors in order starting at the top left corner and go row by row for the whole side: (Use capital letters for colors Ex: YOB = Yellow, Orange, Blue): ');
 # cube[0] = list(string_1);
@@ -28,149 +28,160 @@ cube = [['G','G','G','G','G','G','G','G','G'],['O','O','O','O','O','O','O','O','
 
 def rowTurn(row_numb, direction):
     # 1 = Right and -1 = Left
-    tempList1 = [];
-    tempList2 = [];
-    tempList3 = [];
-    tempList4 = [];
+    tempList1 = []
+    tempList2 = []
+    tempList3 = []
+    tempList4 = []
     if(row_numb == 1):
-        start = 0;
-        end = 3;
+        start = 0
+        end = 3
     elif(row_numb == 2):
         if(direction == 1):
-            rowTurn(1,-1);
-            rowTurn(3,-1);
-            swap(0,3);
-            swap(4,3);
-            swap(3,5);
-            return;
+            rowTurn(1,-1)
+            rowTurn(3,-1)
+            swap(0,3)
+            swap(4,3)
+            swap(3,5)
+            return
         else:
-            rowTurn(1,1);
-            rowTurn(3,1);
-            swap(0,4);
-            swap(3,4);
-            swap(4,5);
-            return;
+            rowTurn(1,1)
+            rowTurn(3,1)
+            swap(0,4)
+            swap(3,4)
+            swap(4,5)
+            return
     else:
-        start = 6;
-        end =9;
+        start = 6
+        end = 9
     # If its the first row then we know the first 3 numbers from each array will be altered
-    if(direction == 1 ) :
+    if(direction == 1):
         for x in range(start,end):
-            tempList1.append(cube[0][x]);
-            tempList2.append(cube[3][x]);
-            tempList3.append(cube[4][x]);
-            tempList4.append(cube[5][x]);
+            tempList1.append(cube[0][x])
+            tempList2.append(cube[3][x])
+            tempList3.append(cube[4][x])
+            tempList4.append(cube[5][x])
         # This copies the first 3 numbers of the sides into lists
-        y=0;
+        y=0
         for x in range(start,end):
-            cube[0][x] = tempList2[y];
-            cube[3][x] = tempList4[y];
-            cube[4][x] = tempList1[y];
-            cube[5][x] = tempList3[y];
-            y+=1;
-        del tempList1[:];
-        del tempList2[:];
-        del tempList3[:];
-        del tempList4[:];
+            cube[0][x] = tempList2[y]
+            cube[3][x] = tempList4[y]
+            cube[4][x] = tempList1[y]
+            cube[5][x] = tempList3[y]
+            y+=1
+        del tempList1[:]
+        del tempList2[:]
+        del tempList3[:]
+        del tempList4[:]
     # Now we do the a similar format for the opposite direction (Left)
-    if(direction == -1 ) :
+    if(direction == -1):
         for x in range(start,end):
-            tempList1.append(cube[0][x]);
-            tempList2.append(cube[3][x]);
-            tempList3.append(cube[4][x]);
-            tempList4.append(cube[5][x]);
+            tempList1.append(cube[0][x])
+            tempList2.append(cube[3][x])
+            tempList3.append(cube[4][x])
+            tempList4.append(cube[5][x])
 
         # This copies the first 3 numbers of the sides into lists
-        y=0;
+        y=0
         for x in range(start,end):
-            cube[0][x] = tempList3[y];
-            cube[3][x] = tempList1[y];
-            cube[4][x] = tempList4[y];
-            cube[5][x] = tempList2[y];
-            y+=1;
+            cube[0][x] = tempList3[y]
+            cube[3][x] = tempList1[y]
+            cube[4][x] = tempList4[y]
+            cube[5][x] = tempList2[y]
+            y+=1
 
-        del tempList1[:];
-        del tempList2[:];
-        del tempList3[:];
-        del tempList4[:];
+        del tempList1[:]
+        del tempList2[:]
+        del tempList3[:]
+        del tempList4[:]
 
-    return;
+    return
 
 def colFlip(col_numb, direction):
     # 1 = Up and -1 = Down
-    tempList1 = [];
-    tempList2 = [];
-    tempList3 = [];
-    tempList4 = [];
+    tempList1 = []
+    tempList2 = []
+    tempList3 = []
+    tempList4 = []
     if(col_numb == 1):
-        index = [0,3,6];
+        index = [0,3,6]
     elif(col_numb == 2):
         if(direction == 1):
-            colFlip(1,-1);
-            colFlip(3,-1);
-            swap(0,2);
-            swap(1,2);
-            swap(2,5);
-            return;
+            colFlip(1,-1)
+            colFlip(3,-1)
+            swap(0,2)
+            swap(1,2)
+            swap(2,5)
+            return
         else:
-            colFlip(1,1);
-            colFlip(3,1);
-            swap(0,1);
-            swap(5,1);
-            swap(5,2);
-            return;
+            colFlip(1,1)
+            colFlip(3,1)
+            swap(0,1)
+            swap(5,1)
+            swap(5,2)
+            return
     else:
-        index = [2,5,8];
+        index = [2,5,8]
     # If its the first row then we know the first 3 numbers from each array will be altered
-    if(direction == 1 ) :
+    if(direction == 1):
         for x in range(3):
-            tempList1.append(cube[0][index[x]]);
-            tempList2.append(cube[1][index[x]]);
-            tempList3.append(cube[2][index[x]]);
-            tempList4.append(cube[5][index[x]]);
+            tempList1.append(cube[0][index[x]])
+            tempList2.append(cube[1][index[x]])
+            tempList3.append(cube[2][index[x]])
+            tempList4.append(cube[5][index[x]])
         # This copies the first 3 numbers of the sides into lists
-        y=0;
+        y=0
         for x in range(3):
-            cube[0][index[x]] = tempList3[y];
-            cube[1][index[x]] = tempList1[y];
-            cube[2][index[x]] = tempList4[y];
-            cube[5][index[x]] = tempList2[y];
-            y+=1;
-        del tempList1[:];
-        del tempList2[:];
-        del tempList3[:];
-        del tempList4[:];
+            cube[0][index[x]] = tempList3[y]
+            cube[1][index[x]] = tempList1[y]
+            cube[2][index[x]] = tempList4[y]
+            cube[5][index[x]] = tempList2[y]
+            y+=1
+        del tempList1[:]
+        del tempList2[:]
+        del tempList3[:]
+        del tempList4[:]
     # Now we do the a similar format for the opposite direction (Left)
-    if(direction == -1 ) :
+    if(direction == -1):
         for x in range(3):
-            tempList1.append(cube[0][index[x]]);
-            tempList2.append(cube[1][index[x]]);
-            tempList3.append(cube[2][index[x]]);
-            tempList4.append(cube[5][index[x]]);
+            tempList1.append(cube[0][index[x]])
+            tempList2.append(cube[1][index[x]])
+            tempList3.append(cube[2][index[x]])
+            tempList4.append(cube[5][index[x]])
 
         # This copies the first 3 numbers of the sides into lists
-        y=0;
+        y=0
         for x in range(3):
-            cube[0][index[x]] = tempList2[y];
-            cube[1][index[x]] = tempList4[y];
-            cube[2][index[x]] = tempList1[y];
-            cube[5][index[x]] = tempList3[y];
-            y+=1;
+            cube[0][index[x]] = tempList2[y]
+            cube[1][index[x]] = tempList4[y]
+            cube[2][index[x]] = tempList1[y]
+            cube[5][index[x]] = tempList3[y]
+            y+=1
 
-        del tempList1[:];
-        del tempList2[:];
-        del tempList3[:];
-        del tempList4[:];
+        del tempList1[:]
+        del tempList2[:]
+        del tempList3[:]
+        del tempList4[:]
 
-    return;
+    return
 
+def faceRotate(level_numb, direction):
+# 1 = Clockwise and -1 = Counter-Clockwise
+    swap(0,2)
+    swap(1,2)
+    swap(2,5)
+
+# Swap opposite faces of the cube (useful for 2nd row turn and 2nd col flip)
 def swap(start,end):
-    cube[start], cube[end] = cube[end], cube[start];
-    return;
+    cube[start], cube[end] = cube[end], cube[start]
+    return
 
-# rowTurn(2,-1);
+# Function that returns which color is on the center of a passed in side
+# The color in the center dictates which color pieces should be on that side 
+def getCenter(side):
+    return cube[side][4]
+
 # Print a specific side for debugging purposes
-# side == (0->front, 3->left, 4->right, 5->back)
+# side == (0->front, 1-> top, 2->bottom, 3->left, 4->right, 5->back)
 def printSide(side):
     print(cube[side][0] + ' ' + cube[side][1] + ' ' + cube[side][2])
     print(cube[side][3] + ' ' + cube[side][4] + ' ' + cube[side][5])
@@ -179,39 +190,31 @@ def printSide(side):
 def printAllSides():
     print("Front Side")
     printSide(0)    # Front side
-    print("=====")
+    print("======")
     print("Top Side")
-    printSide(1)    # Top side?
-    print("=====")
+    printSide(1)    # Top side
+    print("======")
     print("Bottom Side")
-    printSide(2)    # Bottom side?
-    print("=====")
+    printSide(2)    # Bottom side
+    print("======")
     print("Left Side")
     printSide(3)    # Left Side
-    print("=====")
+    print("======")
     print("Right Side")
     printSide(4)    # Right Side
-    print("=====")
+    print("======")
     print("Back Side")
     printSide(5)    # Back Side
-    print("=====")
+    print("======")
 
-printSide(0)    # Front side
-print("=====")
-printSide(1)    # Top side?
-print("=====")
-printSide(2)    # Bottom side?
-print("=====")
-printSide(3)    # Left Side
-print("=====")
-printSide(4)    # Right Side
-print("=====")
-printSide(5)    # Back Side
-print("=====")
+printAllSides()
+faceRotate(1,-1)
+printAllSides()
 
-# colFlip(2,1);
-#rowTurn(2,-1);
+# colFlip(2,-1)
+# rowTurn(2,1)
+# printAllSides()
 # for x in range(len(cube[0])):
-#     print(cube[0][x]);
+#     print(cube[0][x])
 
-# print(cube[1][1]);
+# print(cube[1][1])
